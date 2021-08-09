014cf16b3d8172b2531b302a737929d49e332120
BuildTools <unconfigured@null.spigotmc.org>
2021-08-09 19:56:33 -0400

    Add project files.

---
+++ CSharpProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your name is " + name + "So is mine!");
+            Console.ReadLine();
+        }
+    }
+}
