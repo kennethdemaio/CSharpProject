2bec28c9ad3f90836e83181a35d4afc60a4b0005
BuildTools <unconfigured@null.spigotmc.org>
2021-08-09 20:07:22 -0400

    Added changes

--- CSharpProject/Program.cs
+++ CSharpProject/Program.cs
@@ -9,6 +9,8 @@ namespace CSharpApp
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Your name is " + name + "So is mine!");
+            Console.WriteLine("How's your day");
+            string day = Console.ReadLine();
             Console.ReadLine();
         }
     }
